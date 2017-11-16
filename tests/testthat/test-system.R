76fde765dcbec68173318719f0cefcbc9773ee6d
Dhirendra Singh <dhi.singh@gmail.com>
2017-11-16 13:33:19 +1100

    First system test runs (no output check yet)

--- tests/testthat/test-system.R
+++ tests/testthat/test-system.R
@@ -1,7 +1,7 @@
 context("system tests")
 
 test_that("running system test1", {
-
+  Sys.setenv("R_TESTS" = "") # needed for R CMD CHECK to run correctly
+  offsetsim::run('test01run.R')
   expect_equal( 1, 1 )
-
 })
