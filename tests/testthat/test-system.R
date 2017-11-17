bc11565fc3a1c76cb0204cddb21a6c1a73ab7a47
Dhirendra Singh <dhi.singh@gmail.com>
2017-11-17 12:08:49 +1100

    test01 passes against expected collated_outputs RDS file

--- tests/testthat/test-system.R
+++ tests/testthat/test-system.R
@@ -1,7 +1,12 @@
 context("system tests")
 
-test_that("running system test1", {
+library(tools)
+
+test_that("running test01", {
   Sys.setenv("R_TESTS" = "") # needed for R CMD CHECK to run correctly
+  expected <- md5sum('../expected/test01out/collated_scenario_001_realisation_001_feature_001.rds')
   offsetsim::run('test01run.R', INFO)
-  expect_equal( 1, 1 )
+  actual <- md5sum('../output/test01out/simulation_runs/00001/collated_outputs/collated_scenario_001_realisation_001_feature_001.rds')
+  print(paste('expected[', expected, '] actual[', actual, ']'))
+  expect_true(actual == expected)
 })
